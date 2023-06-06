976a18f92fef1e4750d5646d68d5445193dbab14
pierog <pgarcia@usmpvirtual.edu.pe>
2023-06-06 12:22:07 -0500

    Cambios Api

--- Controllers/MotosApiController.cs
+++ Controllers/MotosApiController.cs
@@ -7,16 +7,15 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Trabajo.DTO;
 using Trabajo.Integration;
-using System.Net.Http.Headers;
 
 namespace Trabajo.Controllers
 {
-    public class TipoCambioController : Controller
+    public class MotosApiController : Controller
     {
-        private readonly ILogger<TipoCambioController> _logger;
+        private readonly ILogger<MotosApiController> _logger;
         private readonly CurrencyExchangeApiIntegration _currency;
 
-        public TipoCambioController(ILogger<TipoCambioController> logger,
+        public MotosApiController(ILogger<MotosApiController> logger,
         CurrencyExchangeApiIntegration currency)
         {
             _logger = logger;
@@ -31,23 +30,6 @@ namespace Trabajo.Controllers
         [HttpPost]
         public async Task<IActionResult> Exchange(TipoCambioDTO? tipoCambio)
         {
-            var client = new HttpClient();
-            var request = new HttpRequestMessage
-            {
-	        Method = HttpMethod.Get,
-	        RequestUri = new Uri("https://motorcycle-specs-database.p.rapidapi.com/article/2012/BMW/F%20800%20GS%20Trophy"),
-	        Headers =
-	            {
-		        { "X-RapidAPI-Key", "89808bcf44mshc2b67244f0dfe73p1342b3jsn471f934c3e23" },
-		        { "X-RapidAPI-Host", "motorcycle-specs-database.p.rapidapi.com" },
-	            },
-            };
-            using (var response = await client.SendAsync(request))
-            {
-	            response.EnsureSuccessStatusCode();
-	            var body = await response.Content.ReadAsStringAsync();
-	            Console.WriteLine(body);
-            }
            double rate = await _currency.GetExchangeRate(tipoCambio.From, tipoCambio.To);
            var cambio = tipoCambio.Cantidad * rate;
            ViewData["rate"] = rate;
---
+++ DTO/MotosApiDTO.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trabajo.DTO
+{
+    public class MotoApiDTO
+    {
+        public int Cantidad { get; set; }
+        public string? From { get; set; }
+        public string? To { get; set; }
+    }
+
+}
