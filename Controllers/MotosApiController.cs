cf17a0c2028a5bfc49cce1df79112a8728e123f4
sora153 <eltrompeb1002@gmail.com>
2023-06-07 23:01:36 -0500

    Otroavancescasifinal

--- Controllers/MotosApiController.cs
+++ Controllers/MotosApiController.cs
@@ -17,55 +17,176 @@ namespace Trabajo.Controllers
 {
     public class MotosApiController : Controller
     {
-         static async Task Main(string[] args){
-            string apiKey = "89808bcf44mshc2b67244f0dfe73p1342b3jsn471f934c3e23";
-        string baseUrl = "https://motorcycle-specs-database.p.rapidapi.com/article";
-            
-            HttpClient client = new HttpClient();
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-            client.DefaultRequestHeaders.Add("X-RapidAPI-Key", apiKey);
+        public async Task<ActionResult> ObtenerDatosDeAPI()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://motorcycle-specs-database.p.rapidapi.com/article/2012/BMW/F%20800%20GS%20Trophy"),
+                    Headers =
+                    {
+                        { "X-RapidAPI-Key", "4caffe0f73msha769e2268018d23p14c5e1jsncc7bec4533f8" },
+                        { "X-RapidAPI-Host", "motorcycle-specs-database.p.rapidapi.com" },
+                    },
+                };
 
-            try{
-                 HttpResponseMessage response = await client.GetAsync(baseUrl + "/2012/BMW/F%20800%20GS%20Trophy");
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
 
-                 if (response.IsSuccessStatusCode){
-                    
-                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    // Aquí puedes procesar los datos de la respuesta de la API
+                    // Puedes almacenarlos en una variable para usarlos en tu vista o hacer cualquier otra operación con ellos
 
-                   MotorcycleSpecsData data = JsonConvert.DeserializeObject<MotorcycleSpecsData>(jsonResponse);
-
-
-                Console.WriteLine($"Modelo: {data.Model}");
-                Console.WriteLine($"Fabricante: {data.Make}");
-                Console.WriteLine($"Año: {data.Year}");
-                Console.WriteLine($"Especificaciones: {data.Specs}");
-
-
-
-                 }else{
-
-                    Console.WriteLine($"Error en la solicitud: {response.StatusCode}");
-                 }
+                    return Content(body);
+                }
             }
-            catch (Exception ex)
-        {
-            Console.WriteLine($"Error: {ex.Message}");
-        }
-        finally
-        {
-            
-            client.Dispose();
         }
 
-         }
+        public async Task<List<MotosApiDTO>> ObtenerMarcasDeMotos()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri("https://motorcycle-specs-database.p.rapidapi.com/make"),
+                    Headers =
+                    {
+                        { "X-RapidAPI-Key", "4caffe0f73msha769e2268018d23p14c5e1jsncc7bec4533f8" },
+                        { "X-RapidAPI-Host", "motorcycle-specs-database.p.rapidapi.com" },
+                    },
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    List<MotosApiDTO> marcas = ProcesarDatos(body);
+
+                    return marcas;
+                }
+            }
+        }
+
+        private List<MotosApiDTO> ProcesarDatos(string responseBody)
+        {
+            dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+            List<MotosApiDTO> marcas = new List<MotosApiDTO>();
+
+            foreach (var item in jsonResponse)
+            {
+                var marca = new MotosApiDTO
+                {
+                    Id = item.id,
+                    Name = item.name
+                };
+
+                marcas.Add(marca);
+            }
+
+            return marcas;
+        }
+
+        private List<MotosApiDTO2> ProcesarDatos2(string responseBody)
+        {
+            dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+            List<MotosApiDTO2> modelos = new List<MotosApiDTO2>();
+
+            foreach (var item in jsonResponse)
+            {
+                var modelos2 = new MotosApiDTO2
+                {
+                    Id = item.id,
+                    Name = item.name
+                };
+
+                modelos.Add(modelos2);
+            }
+
+            return modelos;
+        }
+
+        private List<MotosApiDTO3> ProcesarDatos3(string responseBody)
+        {
+            dynamic jsonResponse = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+            List<MotosApiDTO3> spec = new List<MotosApiDTO3>();
+
+            foreach (var item in jsonResponse)
+            {
+                var modelos3 = new MotosApiDTO3
+                {
+                    Id = item.id,
+                    Name = item.name
+                };
+
+                spec.Add(modelos3);
+            }
+
+            return spec;
+        }
+
+        public async Task<List<MotosApiDTO2>> ObtenerModelosDeMotos(string marca)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://motorcycle-specs-database.p.rapidapi.com/model/make-id/{marca}"),
+                    Headers =
+                    {
+                        { "X-RapidAPI-Key", "4caffe0f73msha769e2268018d23p14c5e1jsncc7bec4533f8" },
+                        { "X-RapidAPI-Host", "motorcycle-specs-database.p.rapidapi.com" },
+                    },
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    List<MotosApiDTO2> modelos = ProcesarDatos2(body);
+
+                    return modelos;
+                }
+            }
+        }
+
+        public async Task<List<MotosApiDTO3>> ObtenerSpecDeMotos(string marca, string modelo)
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri($"https://motorcycle-specs-database.p.rapidapi.com/make/{marca}/model/{modelo}"),
+                    Headers =
+                    {
+                        { "X-RapidAPI-Key", "4caffe0f73msha769e2268018d23p14c5e1jsncc7bec4533f8" },
+                        { "X-RapidAPI-Host", "motorcycle-specs-database.p.rapidapi.com" },
+                    },
+                };
+
+                using (var response = await client.SendAsync(request))
+                {
+                    response.EnsureSuccessStatusCode();
+                    var body = await response.Content.ReadAsStringAsync();
+
+                    List<MotosApiDTO3> spec = ProcesarDatos3(body);
+
+                    return spec;
+                }
+            }
+        }
        
     }
-
-    class MotorcycleSpecsData
-{
-    public string? Model { get; set; }
-    public string? Make { get; set; }
-    public int Year { get; set; }
-    public string? Specs { get; set; }
-}
 }
--- DTO/MotosApiDTO2.cs
+++ DTO/MotosApiDTO2.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Trabajo.DTO
 {
-    public class MotosApiDTO
+    public class MotosApiDTO2
     {
         public int Id { get; set; }
         public string Name { get; set; }
